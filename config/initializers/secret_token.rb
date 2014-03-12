09e173bc61f7f9143710244b1a69bcefff5d5e78
Natsumi Hirano <rubymydear.n@gmail.com>
2014-03-12 10:42:48 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IgnisExercise1::Application.config.secret_key_base = 'f8c14b8a296bd5d6c2d94b5a3965181bd4cec2b56bea92ab2ea9f85425680e25b37a4dfc542830dbac307d34e5b9fc0ceacbe7fd9d85e0aac1ec2319abae339b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IgnisExercise1::Application.config.session_store :cookie_store, key: '_ignis_exercise_1_session'
